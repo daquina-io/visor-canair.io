03779fc3eae71fcd30b6f03fe1341ebfb8eeb51d
son0p <fede2001@gmail.com>
2020-08-01 15:57:25 -0500

    Problem: There is no way to annotate the route
    Solution:load file created in geojson.io whith comment fields on markers

--- app.R
+++ app.R
@@ -19,11 +19,13 @@ ui <- fluidPage(
     sidebarPanel(
 
       # Input: Select a file ----
-      fileInput("file1", "Elija su archivo json",
+      fileInput("file1", "Elija su archivo de recorrido json",
                 multiple = FALSE,
                 accept = c("application/json",
-                         ".json")),
-
+                           ".json")),
+      fileInput("file2", "Elija su archivo de anotaciones csv (creado en http://geojson.io/)",
+                multiple = FALSE,
+                accept = ".csv"),
       # Horizontal line ----
       tags$hr()
 
@@ -49,22 +51,24 @@ set_color <- function(x) {
            "maroon")))))
 }
 
+
+
 ## Define server logic to read selected file ----
 server <- function(input, output, session) {
-
     output$contents <-  renderLeaflet({
-
         ## input$file1 will be NULL initially. After the user selects
         ## and uploads a file, head of that data file by default,
         ## or all rows if selected, will be shown.
-
         req(input$file1)
-
         ## when reading semicolon separated files,
         ## having a comma separator causes `read.csv` to error
         tryCatch(
         {
             df <- fromJSON(input$file1$datapath)
+            dfAnnotations <- read.csv(input$file2$datapath)
+            ##dfAnnotations <- read.csv("~/Data/canairio/points.csv")
+            dfAnnotations <- as.data.frame(dfAnnotations)
+            colnames(dfAnnotations) <- c("color", "size", "symbol", "comment", "lng", "lat")
         },
         error = function(e) {
             ## return a safeError if a parsing error occurs
@@ -77,17 +81,15 @@ server <- function(input, output, session) {
 
         leaflet(df$data) %>%
             addTiles() %>%
-            addCircleMarkers(~lon, ~lat, popup = ~as.character(P25), color = ~color, layerId = ~timestamp)
+            addCircleMarkers(~lon, ~lat, popup = ~as.character(P25), color = ~color, layerId = ~timestamp)  %>%
+            addMarkers(data = dfAnnotations, group = "Alertas", popup = ~comment)
     })
 
     output$dygraph <- renderDygraph({
-
         ## input$file1 will be NULL initially. After the user selects
         ## and uploads a file, head of that data file by default,
         ## or all rows if selected, will be shown.
-
         req(input$file1)
-
         ## when reading semicolon separated files,
         ## having a comma separator causes `read.csv` to error
         tryCatch(
@@ -99,14 +101,10 @@ server <- function(input, output, session) {
             stop(safeError(e))
         }
         )
-
         df$data$time <- as.POSIXct(df$data$timestamp, origin="1970-01-01")
-
         dygraph(xts(df$data$P25, order.by=df$data$time), main = "Exposición material particulado PM2.5", ylab = "μg/m³") %>%
             dySeries( label = "μg/m³") %>%
             dyRangeSelector()
-
-
     })
 }
 
