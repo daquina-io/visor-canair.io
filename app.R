9ee9137753c8dc7d9d7f5501b4e59cebec5f53dd
brolin <brolin108@gmail.com>
2021-09-30 09:15:16 -0500

    Add feature: Button to save the uploaded JSON file in CSV format

--- app.R
+++ app.R
@@ -20,119 +20,124 @@ ui <- fluidPage(
 
       # Input: Select a file ----
       fileInput("file1", "Elija su archivo json",
-                multiple = FALSE,
-                accept = c("application/json",
-                         ".json")),
+        multiple = FALSE,
+        accept = c(
+          "application/json",
+          ".json"
+        )
+      ),
 
       # Horizontal line ----
       tags$hr(),
-
       selectInput(
-          'pollulant', 'Elija el tipo de contaminante a visualizar', choices = c("PM25","CO2") ,
-          selectize = FALSE
+        "pollulant", "Elija el tipo de contaminante a visualizar",
+        choices = c("PM25", "CO2"),
+        selectize = FALSE
       ),
-
-      sliderInput('radius', 'Tamaño del punto', 
-                  min=4, max=30,
-                  value=4),
-
-      a("visor desarrollado por un/loquer", href="https://github.com/daquina-io/visor-canair.io")
-
+      sliderInput("radius", "Tamaño del punto",
+        min = 4, max = 30,
+        value = 4
+      ),
+      a("visor desarrollado por un/loquer", href = "https://github.com/daquina-io/visor-canair.io"),
+      tags$hr(),
+      ## Button
+      downloadButton("downloadCSVData", "Convertir archivo JSON a CSV")
     ),
 
     # Main panel for displaying outputs ----
     mainPanel(
 
       # Output: Data file ----
-        leafletOutput("contents"),
-        dygraphOutput("dygraph")
+      leafletOutput("contents"),
+      dygraphOutput("dygraph")
     )
-
   )
 )
 
 set_color_co2 <- function(x) {
-    ifelse( x < 600 , "green",
-    ifelse( x >= 600 & x < 800 , "yellow",
-    ifelse( x >= 800 & x < 1000, "orange",
-    ifelse( x >= 1000 & x < 1500, "red",
-    ifelse( x >= 1500 & x < 2000, "purple",
-           "maroon")))))
+  ifelse(x < 600, "green",
+    ifelse(x >= 600 & x < 800, "yellow",
+      ifelse(x >= 800 & x < 1000, "orange",
+        ifelse(x >= 1000 & x < 1500, "red",
+          ifelse(x >= 1500 & x < 2000, "purple",
+            "maroon"
+          )
+        )
+      )
+    )
+  )
 }
 
 set_color_p25 <- function(x) {
-    ifelse( x < 13 , "green",
-    ifelse( x >= 13 & x < 35 , "yellow",
-    ifelse( x >= 35 & x < 55, "orange",
-    ifelse( x >= 55 & x < 150, "red",
-    ifelse( x >= 150 & x < 250, "purple",
-           "maroon")))))
+  ifelse(x < 13, "green",
+    ifelse(x >= 13 & x < 35, "yellow",
+      ifelse(x >= 35 & x < 55, "orange",
+        ifelse(x >= 55 & x < 150, "red",
+          ifelse(x >= 150 & x < 250, "purple",
+            "maroon"
+          )
+        )
+      )
+    )
+  )
 }
 
 set_color <- function(pollulant) {
-    if(pollulant == "PM25") return( set_color_p25 )
-    return( set_color_co2 )
+  if (pollulant == "PM25") {
+    return(set_color_p25)
+  }
+  return(set_color_co2)
 }
 
 ## Define server logic to read selected file ----
 server <- function(input, output, session) {
+  df <- reactive({
+    ## input$file1 will be NULL initially. After the user selects
+    ## and uploads a file, head of that data file by default,
+    ## or all rows if selected, will be shown.
 
-    output$contents <-  renderLeaflet({
+    req(input$file1)
 
-        ## input$file1 will be NULL initially. After the user selects
-        ## and uploads a file, head of that data file by default,
-        ## or all rows if selected, will be shown.
+    ## when reading semicolon separated files,
+    ## having a comma separator causes `read.csv` to error
+    tryCatch(
+      {
+        df <- fromJSON(input$file1$datapath)
+      },
+      error = function(e) {
+        ## return a safeError if a parsing error occurs
+        stop(safeError(e))
+      }
+    )
 
-        req(input$file1)
+    df$data <- df$data %>% mutate(color = set_color(input$pollulant)(as.numeric(P25)))
+    df$data$time <- as.POSIXct(df$data$timestamp, origin = "1970-01-01")
 
-        ## when reading semicolon separated files,
-        ## having a comma separator causes `read.csv` to error
-        tryCatch(
-        {
-            df <- fromJSON(input$file1$datapath)
-        },
-        error = function(e) {
-            ## return a safeError if a parsing error occurs
-            stop(safeError(e))
-        }
-        )
+    df
+  })
 
-        df$data <- df$data %>% mutate(color=set_color(input$pollulant)(as.numeric(P25)))
-        df$data$time <- as.POSIXct(df$data$timestamp, origin="1970-01-01")
+  output$contents <- renderLeaflet({
+    leaflet(df()$data) %>%
+      addTiles() %>%
+      addCircleMarkers(~lon, ~lat, popup = ~ as.character(P25), color = ~color, layerId = ~timestamp, radius = ~ input$radius, stroke = FALSE, fillOpacity = 0.5)
+  })
 
-        leaflet(df$data) %>%
-            addTiles() %>%
-            addCircleMarkers(~lon, ~lat, popup = ~as.character(P25), color = ~color, layerId = ~timestamp, radius = ~input$radius, stroke = FALSE, fillOpacity = 0.5)
-    })
+  output$dygraph <- renderDygraph({
+    dygraph(xts(df()$data$P25, order.by = df()$data$time), main = sprintf("Exposición %s", input$pollulant), ylab = ifelse(input$pollulant == "PM25", "μg/m³", "ppm")) %>%
+      dySeries(label = ifelse(input$pollulant == "PM25", "μg/m³", "ppm")) %>%
+      dyRangeSelector()
+  })
 
-    output$dygraph <- renderDygraph({
-
-        ## input$file1 will be NULL initially. After the user selects
-        ## and uploads a file, head of that data file by default,
-        ## or all rows if selected, will be shown.
-
-        req(input$file1)
-
-        ## when reading semicolon separated files,
-        ## having a comma separator causes `read.csv` to error
-        tryCatch(
-        {
-            df <- fromJSON(input$file1$datapath)
-        },
-        error = function(e) {
-            ## return a safeError if a parsing error occurs
-            stop(safeError(e))
-        }
-        )
-
-        df$data$time <- as.POSIXct(df$data$timestamp, origin="1970-01-01")
-
-        dygraph(xts(df$data$P25, order.by=df$data$time), main = sprintf("Exposición %s",input$pollulant), ylab = ifelse(input$pollulant == "PM25", "μg/m³", "ppm")) %>%
-            dySeries( label = ifelse(input$pollulant == "PM25", "μg/m³", "ppm")) %>%
-            dyRangeSelector()
-
-
-    })
+  # Downloadable csv of selected dataset ----
+  output$downloadCSVData <- downloadHandler(
+    filename = function() {
+      paste(df()$name, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(df()$data, file, row.names = FALSE)
+    },
+    contentType = "text/csv"
+  )
 }
 
 ## Create Shiny app ----
